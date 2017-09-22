68b9b1062a02edd3e0d1dbc3436dc57575e33bce
ywCN <wangyouhao90@gmail.com>
2017-09-22 15:06:55 -0400

    parse table and print

--- playground/webScraping/table_scrapping.py
+++ playground/webScraping/table_scrapping.py
@@ -4,3 +4,17 @@ import urllib.request
 sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')  # lxml is parser
 
+# <tr> table rows
+# <th> table header
+# <td> table data
+
+# table = soup.table
+table = soup.find('table')  # same as above
+# print(table)  # contains tags
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
+
