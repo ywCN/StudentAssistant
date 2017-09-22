b5dc2a2db79b253a7bc0c61e34a32266251959d4
ywCN <wangyouhao90@gmail.com>
2017-09-22 15:12:05 -0400

    added panda solution for tables

--- playground/webScraping/table_scrapping.py
+++ playground/webScraping/table_scrapping.py
@@ -1,20 +1,25 @@
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
-sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-soup = bs.BeautifulSoup(sauce, 'lxml')  # lxml is parser
+# sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+# soup = bs.BeautifulSoup(sauce, 'lxml')  # lxml is parser
+#
+# # <tr> table rows
+# # <th> table header
+# # <td> table data
+#
+# # table = soup.table
+# table = soup.find('table')  # same as above
+# # print(table)  # contains tags
+# table_rows = table.find_all('tr')
+#
+# for tr in table_rows:
+#     td = tr.find_all('td')
+#     row = [i.text for i in td]
+#     print(row)
 
-# <tr> table rows
-# <th> table header
-# <td> table data
-
-# table = soup.table
-table = soup.find('table')  # same as above
-# print(table)  # contains tags
-table_rows = table.find_all('tr')
-
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    print(row)
+dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/')  # parse all tables it can find
+for df in dfs:
+    print(df)
 
