ba7fd34c19fcc05b493d056374b5b742a143f599
ywCN <wangyouhao90@gmail.com>
2017-09-22 16:23:14 -0400

    worked but having some extra info

--- playground/webScraping/dynamic_scrapping.py
+++ playground/webScraping/dynamic_scrapping.py
@@ -1,29 +1,35 @@
+import bs4 as bs
 import sys  # PyQT needs this to take system arguments
+import urllib.request
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QUrl
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
-import bs4 as bs
-import urllib.request
 
-
-class Client(QWebEnginePage) :
-
+class Page(QWebEnginePage):
     def __init__(self, url):
         self.app = QApplication(sys.argv)
         QWebEnginePage.__init__(self)
-        self.loadFinished.connect(self.on_page_load)
+        self.html = ''
+        self.loadFinished.connect(self._on_load_finished)
+        self.load(QUrl(url))
+        self.app.exec_()
 
-    def on_page_load(self):
+    def _on_load_finished(self):
+        self.html = self.toHtml(self.Callable)
+        print('Load finished')
+
+    def Callable(self, html_str):
+        self.html = html_str
         self.app.quit()
 
 
-url = 'https://pythonprogramming.net/parsememcparseface/'
-client_reponse = Client(url)
-source = client_reponse.mainFrame().toHtml()  # get mainframe and convert to html
+def main():
+    page = Page('https://pythonprogramming.net/parsememcparseface/')
+    soup = bs.BeautifulSoup(page.html, 'html.parser')
+    js_test = soup.find('p', class_='jstest')
+    print(js_test.text)
 
-soup = bs.BeautifulSoup(source, 'lxml')
-js_test = soup.find('p', class_='jstest')
-print(js_test.text)
+if __name__ == '__main__': main()
 
 #  pretend we are a client or browser so we can run JS
 
