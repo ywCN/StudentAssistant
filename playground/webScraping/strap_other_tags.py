67a00177cadadffc3e948efe12633fd8360b9300
ywCN <wangyouhao90@gmail.com>
2017-09-21 23:16:41 -0400

    finished content scrapping

--- playground/webScraping/strap_other_tags.py
+++ playground/webScraping/strap_other_tags.py
@@ -4,14 +4,19 @@ import urllib.request
 sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
-nav = soup.nav
+nav = soup.nav  # slice from <nav> to </nav>
 
 # print(nav)
 
 # for url in nav.find_all('a'):
 #     print(url.get('href'))  # all the links in nav bar
 
-body = soup.body
-for paragraph in body.find_all('p'):
-    print(paragraph.text)  # only get the text data in body part; enough for most strapping cases
+# body = soup.body
+# for paragraph in body.find_all('p'):
+#     print(paragraph.text)  # only get the text data in body part; enough for most strapping cases
 
+# for div in soup.find_all('div'):
+#     print(div.text)  # get too much info!
+
+for div in soup.find_all('div', class_='body'):
+    print(div.text)  # good info
