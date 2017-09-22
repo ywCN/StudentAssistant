081faf54c44a3d2782f9b3377b18577fbeba45d5
ywCN <wangyouhao90@gmail.com>
2017-09-21 23:12:34 -0400

    text scrapping

--- playground/webScraping/strap_other_tags.py
+++ playground/webScraping/strap_other_tags.py
@@ -8,5 +8,10 @@ nav = soup.nav
 
 # print(nav)
 
-for url in nav.find_all('a'):
-    print(url.get('href'))  # all the links in nav bar
+# for url in nav.find_all('a'):
+#     print(url.get('href'))  # all the links in nav bar
+
+body = soup.body
+for paragraph in body.find_all('p'):
+    print(paragraph.text)  # only get the text data in body part; enough for most strapping cases
+
