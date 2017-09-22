fc024888be939f408a777eed8deb4b232bd1305d
ywCN <wangyouhao90@gmail.com>
2017-09-21 22:59:36 -0400

    tested urls

--- playground/webScraping/strap_test.py
+++ playground/webScraping/strap_test.py
@@ -11,8 +11,14 @@ soup = bs.BeautifulSoup(sauce, 'lxml')  # lxml is parser
 # print(soup.title.name)  # title
 # print(soup.title.string)  # Python Programming Tutorials
 # print(soup.find_all('p'))  # find all paragraph tags
-for paragraph in soup.find_all('p'):
-    # print(paragraph)  # contains tags
-    # print(paragraph.string)  # missing some strings
-    print(paragraph.text)  # use this in most cases
+# for paragraph in soup.find_all('p'):
+#     # print(paragraph)  # contains tags
+#     # print(paragraph.string)  # missing some strings
+#     print(paragraph.text)  # use this in most cases
 
+# print(soup.get_text())  # find all paragraphs
+
+for url in soup.find_all('a'):
+    # print(url)  # get entire tag
+    # print(url.text)  # wrong!
+    print(url.get('href'))  # right way to get urls
