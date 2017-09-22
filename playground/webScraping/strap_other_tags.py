8130d878f05d0b7370062a6e606251d6e785611a
ywCN <wangyouhao90@gmail.com>
2017-09-21 23:09:47 -0400

    nav bar

--- playground/webScraping/strap_other_tags.py
+++ playground/webScraping/strap_other_tags.py
@@ -4,3 +4,9 @@ import urllib.request
 sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
+nav = soup.nav
+
+# print(nav)
+
+for url in nav.find_all('a'):
+    print(url.get('href'))  # all the links in nav bar
