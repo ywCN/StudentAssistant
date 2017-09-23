c97f1e100d824a36cf3631c526bc6e0a1bf8c6d5
ywCN <wangyouhao90@gmail.com>
2017-09-22 22:48:31 -0400

    reviewed parse tables

--- playground/webScraping/table_scrapping.py
+++ playground/webScraping/table_scrapping.py
@@ -2,26 +2,26 @@ import bs4 as bs
 import urllib.request
 import pandas as pd
 
-# sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-# soup = bs.BeautifulSoup(sauce, 'lxml')  # lxml is parser
-sauce = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()  # do not add / at end
-soup = bs.BeautifulSoup(sauce, 'xml')
-for url in soup.find_all('loc'):
-    print(url.text)
+sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')  # lxml is parser
+# sauce = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()  # do not add / at end
+# soup = bs.BeautifulSoup(sauce, 'xml')
+# for url in soup.find_all('loc'):
+#     print(url.text)
 #
 # # <tr> table rows
 # # <th> table header
 # # <td> table data
 #
-# # table = soup.table
+table = soup.table
 # table = soup.find('table')  # same as above
 # # print(table)  # contains tags
-# table_rows = table.find_all('tr')
-#
-# for tr in table_rows:
-#     td = tr.find_all('td')
-#     row = [i.text for i in td]
-#     print(row)
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
 
 # # dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/')  # parse all tables it can find
 # dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/', header=0)  # make first row the header
