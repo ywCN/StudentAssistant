b27235f587fb5714c7c6e649379888d4e6abe531
ywCN <wangyouhao90@gmail.com>
2017-09-22 23:56:25 -0400

    trying to print rows

--- playground/webScraping/try_scraping.py
+++ playground/webScraping/try_scraping.py
@@ -1,23 +1,25 @@
 from selenium import webdriver
-import getpass
+import getpass  # this does not work well for some reasons
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 import bs4 as bs
+import pandas as pd
 
 def get_user_password():
-    user = input("user name:")
-    password = getpass.getpass("password:")
+    user = input("Enter the User Name:")
+    password = input("Enter the Password:")
+    # print("got password")
     return user, password
 
 def do_things():
     info = get_user_password()
     driver = webdriver.PhantomJS("C:\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
     driver.get("https://mystevens.stevens.edu/sso/web4student.php")
-    time.sleep(5)
+    time.sleep(2)
     driver.find_element_by_name("j_username").send_keys(info[0])
     driver.find_element_by_name("j_password").send_keys(info[1])
     driver.find_element_by_name('submit').submit()
-    time.sleep(5)
+    time.sleep(2)
     hover1_element = driver.find_element_by_id("menuHeading5")
     # print(hover1_element)
     hover2_element = driver.find_element_by_xpath("//div[a/@title='Course Sections']")
@@ -30,33 +32,41 @@ def do_things():
     time.sleep(2)
     hover3 = ActionChains(driver).click(hover2_element)
     hover3.perform()
-    time.sleep(5)
+    time.sleep(2)
     majors = driver.find_elements_by_xpath("//select[option]")
     # print(type(classes))  # it is a list
     majors[0].click()  # test one object in the list
     driver.find_element_by_name('submitbutton').submit()
-    time.sleep(5)
+    time.sleep(2)
     courses = driver.find_elements_by_xpath("//select[option]")
     courses[0].click()
     driver.find_element_by_name('submitbutton').submit()
-    time.sleep(5)
-    print("no problem!")
-    # select = Select(driver.find_element_by_name('Subject'))
-    # select.select_by_visible_text('Biomedical Engineering')
-    # driver.find_element_by_id('submitbutton').submit()
-    # page = driver.find_element_by_id('option value')
-    # print(page.text)
+    time.sleep(2)
+    # print("no problem!")
+    # parse(driver.page_source)
+    page = driver.find_elements_by_xpath("//tr[td/@class='dddefault']")  # dddefaultcenter
+    print(page.text)
 
 
 def parse(html):
-    soup = bs.BeautifulSoup(html, 'lxml')
-    table = soup.table
-    table_rows = table.find_all('tr')
+    # soup = bs.BeautifulSoup(html, 'lxml')
+    # for table in soup.find_all('table'):
+    #     for subtable in table.find_all('table'):
+    #         for row in subtable:
+    #             print(row)
 
-    for tr in table_rows:
-        td = tr.find_all('td')
-        row = [i.text for i in td]
-        print(row)
+    # table = soup.table
+    # table_rows = table.find_all('tr')
+    # print(table_rows)
+    # for tr in table_rows:
+    #     td = tr.find_all('td')
+    #     row = [i.text for i in td]
+    #     print(row)
+    #
+    # print("-----------------------------------------------------------------------------------------")
+    # dfs = pd.read_html(html, header=0)  # make first row the header
+    # for df in dfs:
+    #     print(df)
 
 
 do_things()
