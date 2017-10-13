a228e88c57227e2d6ea0742cae77d8df0f989b00
wangyouhao <wangyouhao90@gmail.com>
2017-10-12 22:51:46 -0400

    OOD

--- parse course dependencies/parse pdf.py
+++ parse course dependencies/parse pdf.py
@@ -1,39 +1,53 @@
 import re
+'''
+This program will parse as many as possible dependencies from the file.
+If no pre-requisite and nor co-requisite is found, the course has no dependencies with others. 
+However, the course my still need permissions from instructor.
 
-f = open(r'catalog.txt', encoding='utf-8')
+The pdf file is firstly converted into a text file. Then this program will parse dependencies in it.
+'''
 
-re1 = '((?:[a-z][a-z]+))'  # Word 1
-re2 = '( )'  # White Space 1
-re3 = '(\\d+)'  # Integer Number 1
 
-rg = re.compile(re1 + re2 + re3, re.IGNORECASE | re.DOTALL)
+class ParsePDF:
 
-cache = ''
+    def __init__(self):
+        self.file_name = r'catalog.txt'
+        self.file = self.open_file()
 
-for line in f:
-    flag = False
-    m = rg.search(line[:6])
-    if "Prerequisites: " in line and cache != '':
-        # if line.startswith("Prerequisites: ") :
-        #     print(cache)
-        #     cache = ''
-        #     print(line.strip())
-        # else:
-        print(cache)
+    def open_file(self):
+        # try:
+        return open(self.file_name, encoding='utf-8')
+
+    def is_valid_line(self, text):
+        re1 = '((?:[a-z][a-z]+))'  # Word 1
+        re2 = '( )'  # White Space 1
+        re3 = '(\\d+)'  # Integer Number 1
+
+        rg = re.compile(re1 + re2 + re3, re.IGNORECASE | re.DOTALL)
+        return rg.search(text)
+
+    def parse_file(self):
         cache = ''
-        loc = line.index("Prerequisites: ")
-        parsed_line = line[loc:]
-        print(parsed_line.strip())
-    elif "Corequisites: " in line and cache != '':
-        # if line.startswith("Corequisites: "):
-        #     print(cache)
-        #     cache = ''
-        #     print(line.strip())
-        # else:
-        print(cache)
-        cache = ''
-        loc = line.index("Corequisites: ")
-        parsed_line = line[loc:]
-        print(parsed_line.strip())
-    elif m and len(line) < 10 and 'or' not in line:
-        cache = line.strip()
+        for line in self.file:
+            if "Prerequisites: " in line and cache != '':
+                print(cache)
+                cache = ''
+                loc = line.index("Prerequisites: ")
+                parsed_line = line[loc:]
+                print(parsed_line.strip())
+            elif "Corequisites: " in line and cache != '':
+                print(cache)
+                cache = ''
+                loc = line.index("Corequisites: ")
+                parsed_line = line[loc:]
+                print(parsed_line.strip())
+            elif self.is_valid_line(line[:6]) and len(line) < 10 and 'or' not in line:
+                cache = line.strip()
+
+def main():
+    demo = ParsePDF()
+    demo.parse_file()
+
+
+if __name__ == '__main__':
+    main()
