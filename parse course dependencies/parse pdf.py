fd4e8f241f00080f879c9a7144f604d76e3d9364
wangyouhao <wangyouhao90@gmail.com>
2017-10-12 21:02:37 -0400

    read converted text file

--- parse course dependencies/parse pdf.py
+++ parse course dependencies/parse pdf.py
@@ -1,8 +1,9 @@
 
 
 # class ParsePDF:
-f = open(r'catalog.txt')
+f = open(r'catalog.txt', encoding='utf-8')
+
 for line in f:
     if "Prerequisites: " in line or "Corequisites: " in line:
         print(line)
-        
+
