1000d7de178ca927247ee596654d6e6e4edcfda9
wangyouhao <wangyouhao90@gmail.com>
2017-10-12 22:39:49 -0400

    got all non-corrupted dependencies

--- parse course dependencies/parse pdf.py
+++ parse course dependencies/parse pdf.py
@@ -7,25 +7,33 @@ re2 = '( )'  # White Space 1
 re3 = '(\\d+)'  # Integer Number 1
 
 rg = re.compile(re1 + re2 + re3, re.IGNORECASE | re.DOTALL)
-cache1 = ''
-cache2 = ''
+
+cache = ''
+
 for line in f:
     flag = False
     m = rg.search(line[:6])
-    if "Prerequisites: " in line:
-        if line.startswith("Prerequisites: "):
-            print
-            print(line.strip())
-        else:
-            loc = line.index("Prerequisites: ")
-            parsed_line = line[loc:]
-            print(parsed_line.strip())
-    elif "Corequisites: " in line:
-        if line.startswith("Corequisites: "):
-            print(line.strip())
-        else:
-            loc = line.index("Corequisites: ")
-            parsed_line = line[loc:]
-            print(parsed_line.strip())
+    if "Prerequisites: " in line and cache != '':
+        # if line.startswith("Prerequisites: ") :
+        #     print(cache)
+        #     cache = ''
+        #     print(line.strip())
+        # else:
+        print(cache)
+        cache = ''
+        loc = line.index("Prerequisites: ")
+        parsed_line = line[loc:]
+        print(parsed_line.strip())
+    elif "Corequisites: " in line and cache != '':
+        # if line.startswith("Corequisites: "):
+        #     print(cache)
+        #     cache = ''
+        #     print(line.strip())
+        # else:
+        print(cache)
+        cache = ''
+        loc = line.index("Corequisites: ")
+        parsed_line = line[loc:]
+        print(parsed_line.strip())
     elif m and len(line) < 10 and 'or' not in line:
-        print(line.strip())
+        cache = line.strip()
