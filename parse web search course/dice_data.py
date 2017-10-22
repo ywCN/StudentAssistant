a61266802c8f3da1b50b415c050910e7eae330c8
wangyouhao <wangyouhao90@gmail.com>
2017-10-22 18:53:41 -0400

    set up for splitting

--- parse web search course/dice_data.py
+++ parse web search course/dice_data.py
@@ -37,8 +37,10 @@ class DiceData:
     def __init__(self):
         self.db_name = r'courses.db'
         if os.path.isfile(self.db_name):
-            self.conn = sqlite3.connect(r'courses.db')
-            self.c = self.conn.cursor()
+            self.conn1 = sqlite3.connect(self.db_name)  # old db
+            self.c1 = self.conn1.cursor()
+            self.conn2 = sqlite3.connect(r'courses2.db')  # new db
+            self.c2 = self.conn1.cursor()
         else:
             print("Please put %s in the same path of this .py file." % self.db_name)
             exit()
@@ -48,21 +50,20 @@ class DiceData:
         query = 'select courses.SectionTitle from courses'
         print(type(self.query_info(query)))
 
-    def query_info(self, query):
+    def query_info(self, query):  # in old database
         """
         :type query: str
         :rtype: List[List[str]]
         """
-        self.c.execute("{}".format(query))
-        return self.c.fetchall()
+        self.c1.execute("{}".format(query))
+        return self.c1.fetchall()
 
-    def create_table(self):
+    def create_table(self):  # in new database
+        self.c2.execute("CREATE TABLE IF NOT EXISTS courses(CourseID TEXT, CourseName TEXT, CourseSection TEXT, CallNumber TEXT, Status TEXT, Seats TEXT, Day TEXT, Time TEXT, Campus TEXT, Location TEXT, Instructor TEXT, StartDate TEXT, EndDate TEXT, MinCredit TEXT, MaxCredit TEXT)")
 
-        self.c.execute("CREATE TABLE IF NOT EXISTS courses(CourseID TEXT, CourseName TEXT, CourseSection TEXT, CallNumber TEXT, Status TEXT, Seats TEXT, Day TEXT, Time TEXT, Campus TEXT, Location TEXT, Instructor TEXT, StartDate TEXT, EndDate TEXT, MinCredit TEXT, MaxCredit TEXT)")
-
-    def insert_entry(self, data):
-        self.c.execute("INSERT INTO courses (CourseID, CourseName, CourseSection, CallNumber, Status, Seats, Day, Time, Campus, Location, Instructor, StartDate, EndDate, MinCredit, MaxCredit VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
-        self.conn.commit()
+    def insert_entry(self, data):  # in new database
+        self.c2.execute("INSERT INTO courses (CourseID, CourseName, CourseSection, CallNumber, Status, Seats, Day, Time, Campus, Location, Instructor, StartDate, EndDate, MinCredit, MaxCredit VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+        self.conn2.commit()
 
 
 def main():
