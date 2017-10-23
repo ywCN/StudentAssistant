45d0a035365e4755b7a8501766ff0d69d5e984a3
wangyouhao <wangyouhao90@gmail.com>
2017-10-23 11:16:03 -0400

    index issue

--- parse course dependencies/parse pdf.py
+++ parse course dependencies/parse pdf.py
@@ -33,22 +33,22 @@ class ParsePDF:
     def parse_file(self):
         cache = ''
         dep = {}
-        #TODO: return a dict {course: {pre:[dep], co:[dep]}}
+        #TODO: return a dict {course: {pre:str, co:str}}
         for line in self.file:
             if "Prerequisites: " in line and cache != '':
-                print(cache)
+                # print(cache)
 
                 loc = line.index("Prerequisites: ")
                 parsed_line = line[loc:]
-                print(parsed_line.strip())
-                self.parse_line(parsed_line.strip(), dep, cache)
+                # print(parsed_line.strip())
+                self.save_info(parsed_line.strip(), dep, cache)
                 cache = ''
             elif "Corequisites: " in line and cache != '':
-                print(cache)
+                # print(cache)
 
                 loc = line.index("Corequisites: ")
                 parsed_line = line[loc:]
-                print(parsed_line.strip())
+                # print(parsed_line.strip())
 
                 cache = ''
             elif self.is_valid_line(line[:7]) and len(line) < 10 and 'or' not in line:
@@ -56,16 +56,35 @@ class ParsePDF:
             else:
                 pass
 
-    def parse_line(self, line, dep, course):
+        print(dep)
+
+    def save_info(self, line, dep, course):
         """
 
         :param data:
         :return: None
         """
-        pre = []
-        co = []
         # TODO: parse line and put data into dict
         dep[course] = {}
+        reqs = self.parse_line(line)
+        dep[course]['Prerequisites'] = reqs[0]
+        dep[course]['Corequisites'] = reqs[1]
+
+    def parse_line(self, line):
+        pre = ''
+        co = ''
+        if 'Prerequisites' in line and 'Corequisites' in line:
+            data = line.split(' Corequisites: ')
+            co = data[1]
+            pre = data[0].replace('Prerequisites: ', '')
+        elif 'Prerequisites' in line:
+            pre = line.replace('Prerequisites: ', '')
+        elif 'Corequisites' in line:
+            co = line.replace('Corequisites: ', '')
+        else:
+            print('missing lines', line)
+        return pre, co
+
 
 
 
