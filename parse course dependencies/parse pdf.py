0f7c8650483f2c33a8c588a8c02e9c10013223bd
wangyouhao <wangyouhao90@gmail.com>
2017-10-23 11:37:25 -0400

    removed \xa0

--- parse course dependencies/parse pdf.py
+++ parse course dependencies/parse pdf.py
@@ -1,5 +1,7 @@
 import re
 import sqlite3
+import os
+
 '''
 This program will parse as many as possible dependencies from the file.
 If no pre-requisite and nor co-requisite is found, the course has no dependencies with others. 
@@ -12,14 +14,23 @@ The pdf file is firstly converted into a text file. Then this program will parse
 class ParsePDF:
 
     def __init__(self):
-        self.file_name = r'catalog.txt'
+        self.text = r'catalog.txt'
         self.file = self.open_file()
+        self.db = r'course_dependencies.db'
+
+        if os.path.isfile(self.db):
+            print("Please delete or rename {} and run this program again.".format(self.db))
+            exit()
+        else:
+            self.conn2 = sqlite3.connect(self.db)  # new db
+            self.c2 = self.conn2.cursor()
+            self.create_table()
 
     def open_file(self):
         try:
-            return open(self.file_name, encoding='utf-8')
+            return open(self.text, encoding='utf-8')
         except FileNotFoundError:
-            print("Please put {} in the same path!".format(self.file_name))
+            print("Please put {} in the same path!".format(self.text))
             exit()
 
     def is_valid_line(self, text):
@@ -33,51 +44,45 @@ class ParsePDF:
     def parse_file(self):
         cache = ''
         dep = {}
-        #TODO: return a dict {course: {pre:str, co:str}}
         for line in self.file:
             if "Prerequisites: " in line and cache != '':
-                # print(cache)
-
                 loc = line.index("Prerequisites: ")
                 parsed_line = line[loc:]
-                # print(parsed_line.strip())
                 self.save_info(parsed_line.strip(), dep, cache)
                 cache = ''
             elif "Corequisites: " in line and cache != '':
-                # print(cache)
-
                 loc = line.index("Corequisites: ")
                 parsed_line = line[loc:]
-                # print(parsed_line.strip())
-
+                self.save_info(parsed_line.strip(), dep, cache)
                 cache = ''
             elif self.is_valid_line(line[:7]) and len(line) < 10 and 'or' not in line:
                 cache = line.strip()
             else:
-                pass
+                pass  # skip invalid lines
 
-        print(dep)
+        return dep
 
     def save_info(self, line, dep, course):
         """
-
         :param data:
         :return: None
         """
-        # TODO: parse line and put data into dict
         dep[course] = {}
-        reqs = self.parse_line(line)
+        reqs = self.parse_line(line.replace(u'\xa0', u' '))
         dep[course]['Prerequisites'] = reqs[0]
         dep[course]['Corequisites'] = reqs[1]
 
     def parse_line(self, line):
+        """
+        :param line: str
+        :return: (str, str)
+        """
         pre = ''
         co = ''
 
         if 'Prerequisites: Corequisites:' in line:
             co = line.replace('Prerequisites: Corequisites: ', '')
         elif 'Prerequisites: ' in line and 'Corequisites: ' in line:
-            print(line)
             data = line.split(' Corequisites: ')
             co = data[1]
             pre = data[0].replace('Prerequisites: ', '')
@@ -87,12 +92,27 @@ class ParsePDF:
             co = line.replace('Corequisites: ', '')
         else:
             print('missing lines', line)
+
         return pre, co
+
+    def create_table(self):
+        self.c2.execute("CREATE TABLE IF NOT EXISTS dependencies(CourseID TEXT, Prerequisites TEXT, Corequisites TEXT)")
+
+    def insert_entry(self, data):
+        self.c2.execute("INSERT INTO dependencies (CourseID, Prerequisites, Corequisites) VALUES (?, ?, ?)",
+                        (data[0], data[1], data[2]))
+        self.conn2.commit()
+
+    def save_into_db(self):
+        dep = self.parse_file()
+        for key in dep:
+            print(dep[key])
+
 
 
 def main():
     demo = ParsePDF()
-    demo.parse_file()
+    demo.save_into_db()
 
 
 if __name__ == '__main__':
