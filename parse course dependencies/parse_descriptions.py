36d7f91d438e4a7db654f7cfe8af43ed8b31d417
wangyouhao <wangyouhao90@gmail.com>
2017-10-24 11:20:18 -0400

    use regex instead to extract info

--- parse course dependencies/parse_descriptions.py
+++ parse course dependencies/parse_descriptions.py
@@ -17,10 +17,6 @@ class ParseDescription:
         self.text = r'catalog.txt'
         self.file = self.open_file()
         self.db = r'course_descriptions.db'
-        self.course_db = r'courses2.db'  # get ids from this db
-        self.conn2 = sqlite3.connect(self.course_db)  # new db
-        self.c2 = self.conn2.cursor()
-        self.ids = self.get_all_ids()
 
     #     if os.path.isfile(self.db):
     #         print("Please delete or rename {} and run this program again.".format(self.db))
@@ -44,58 +40,35 @@ class ParseDescription:
             print("Please put {} in the same path!".format(self.text))
             exit()
 
-    def get_all_ids(self):
-        query = 'select courses.CourseID from courses'
-        calls = self.query_info(query)
-        ids = set()
-        for call in calls:
-            ids.add(call[0])
-        # print('CS 546' in ids)  # True
-        return ids
-
-    def query_info(self, query):  # in old database
-        """
-        :type query: str
-        :rtype: List[List[str]]
-        """
-        self.c2.execute("{}".format(query))
-        return self.c2.fetchall()
-
     def print_info(self):  # line.replace(u'\xa0', u' ')
+
+        re1 = '((?:[a-z][a-z0-9_]*))'  # Variable Name 1
+        re2 = '(\\s+)'  # White Space 1
+        re3 = '(\\d+)'  # Integer Number 1
+
+        rg = re.compile(re1 + re2 + re3, re.IGNORECASE | re.DOTALL)
+
         cache = []  # CourseID, , Course Name, ,(0-0-0), mutiple lines, ,
         flag = False  # init
         # TODO: start caching line when encountering courseID, stop caching when encounting courseID
         for line in self.file:
             ln = line.strip().replace(u'\xa0', u' ')
-            if ln in self.ids and not flag:
+            m = rg.search(ln)
+            if m and not flag:
                 # print(ln)  # got all ids
                 cache.append(ln)
                 flag = True
-            elif ln in self.ids and flag:
-                # TODO: check cache, if valid save(print)
-                try:
-                    if len(cache[1]) == 0 and len(cache[3]) == 0 and len(cache[4]) == len('(4-0-4)'):
-                        print(cache)
-                except IndexError:
-                    print(cache)
-                # print(cache)
+            elif m and flag:
+                if len(cache) > 5 and len(cache[1]) == 0 and len(cache[3]) == 0 and '(' in cache[4]:
+                    # print(cache)
+                    course_id = cache[0]
+                    course_description = ' '.join(cache[6:])
+                    print(course_id)
+                    print(course_description)
                 cache.clear()
                 cache.append(ln)
             else:
                 cache.append(ln)
-
-        # if [3] length != len((4-0-4)) abandon cache
-
-        # In the cache:
-        # if [0]
-        # if line length > 1, abandon cache
-        # if line length > 70, abandon cache
-        # if line length > 1, abandon cache
-        # while line length < 70, cache line
-
-
-
-
 
 
 def main():
