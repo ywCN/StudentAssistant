697c050c8d977ba4a247744b87eb9de0697fb9dd
djackso2 <djackso2@stevens.edu>
2017-11-07 19:18:13 -0600

    Removing unused imports

--- application/samainapp.py
+++ application/samainapp.py
@@ -2,11 +2,8 @@ from kivy.app import App
 from kivy.network.urlrequest import UrlRequest
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
-#from kivy.uix.floatlayout import FloatLayout
-#from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
-#from kivy.uix.checkbox import CheckBox
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
