8fc8235c50da91fa2edb561c7fa428791e109dff
wangyouhao <wangyouhao90@gmail.com>
2017-11-10 19:58:15 -0500

    added comments for code review

--- parse web search course/scrap_myStevens.py
+++ parse web search course/scrap_myStevens.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
-import getpass  # this does not work in PyCharm
 from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 
@@ -16,109 +15,113 @@ class ScrapStevensCourses:
 
     def __init__(self):
         self.driver = webdriver.PhantomJS("C:\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
-        self.driver.implicitly_wait(10)  # DO NOT CHANGE THIS LINE.
+        self.driver.implicitly_wait(10)  # wait up to 10 second for the targeting element
         self.driver.get("https://mystevens.stevens.edu/sso/web4student.php")
-        self.raw_courses = open('all_courses_raw.txt', 'w+')
-        self.errors = open('errors.txt', 'w+')
-
-    def get_login_info(self):
-        user = input("Enter the User Name:")
-        password = input("Enter the Password:")  # for IDE
-        # password = getpass.getpass()  # for terminal
-        print("got password")
-        return user, password
+        self.raw_courses = open('all_courses_raw.txt', 'w+')  # output
+        self.errors = open('errors.txt', 'w+')  # for unusual cases
 
     def login(self):
-        info = self.get_login_info()
-        self.driver.find_element_by_name("j_username").send_keys(info[0])
-        self.driver.find_element_by_name("j_password").send_keys(info[1])
+        self.driver.find_element_by_name("j_username").send_keys(input("Enter the User Name:"))
+        self.driver.find_element_by_name("j_password").send_keys(input("Enter the Password:"))
         self.driver.find_element_by_name('submit').submit()
 
     def go_to_courses_description_page(self, course_id):
-        select1 = Select(self.driver.find_element_by_xpath('//select[option/@value="%s"]' % course_id))
-        select1.select_by_value(course_id)
-
+        select = Select(self.driver.find_element_by_xpath('//select[option/@value="%s"]' % course_id))
+        select.select_by_value(course_id)
         self.driver.find_element_by_name("submitbutton").submit()
-        # time.sleep(2)
 
     def go_to_courses_page(self, major_id):
         select = Select(self.driver.find_element_by_xpath('//select[option/@value="%s"]' % major_id))
         select.select_by_value(major_id)
         self.driver.find_element_by_name("submitbutton").submit()
-        # time.sleep(2)
 
     def go_to_majors_page(self):
         hover1_element = self.driver.find_element_by_id("menuHeading5")
         hover2_element = self.driver.find_element_by_xpath("//div[a/@title='Course Sections']")
-        ActionChains(self.driver).move_to_element(hover1_element).move_to_element(hover2_element).click(
-            hover2_element).perform()
+        ActionChains(self.driver)\
+            .move_to_element(hover1_element)\
+            .move_to_element(hover2_element)\
+            .click(hover2_element).perform()
         time.sleep(2)
 
     def get_raw_majors(self):
+        """
+        write raw page source into a file.
+        :return: none
+        """
         f = open('majors_raw.txt', 'w+')
         f.write(self.driver.page_source)
         f.close()
 
     def parse_raw_majors(self):
-        self.get_raw_majors()
+        """
+        Return a list of major names for future navigation.
+        :param : str
+        :return: List
+        """
+        self.get_raw_majors()  # need to close it before using the file again
         target = "<option value="
         file = open('majors_raw.txt')
-        # majors = {}
         majors = []
         for line in file:
             if target in line:
                 words = line.strip().split("\"")
                 majors.append(words[1])
-                # majors[words[1]] = words[2][1:]
-                # print(words[1], words[2][1:])
         return majors
 
     def get_raw_courses(self, major):
+        """
+        write raw page source into a file.
+        :param major:
+        :return: none
+        """
         f = open('%s_raw.txt' % major, 'w+')
         f.write(self.driver.page_source)
         f.close()
 
     def parse_raw_courses(self, major):
-        self.get_raw_courses(major)
+        """
+        Return a list of course names for future navigation.
+        :param major: str
+        :return: List
+        """
+        self.get_raw_courses(major)  # need to close it before using the file again
         target = "<option value="
         file = open('%s_raw.txt' % major)
-        # courses = {}
         courses = []
         for line in file:
             if target in line:
                 words = line.strip().split("\"")
                 courses.append(words[1])
-                # courses[words[1]] = words[2][1:]
+        file.close()
         return courses
 
     def save_tables(self):
 
         dfs = pd.read_html(self.driver.page_source)
-        # print(type(dfs))  # <class 'list'>
-        # print(len(dfs[4]))  # [4] is the table we need
         try:
-            line = dfs[4].to_csv(sep=' ', index=False, header=False)
-            line = line.replace(u'\xa0', u' ')
+            line = dfs[4].to_csv(sep=' ', index=False, header=False)  # this is the table we need
+            line = line.replace(u'\xa0', u' ')  # remove invalid character
             self.raw_courses.write(line)
-        except IndexError:
+        except IndexError:  # just in case
             for line in dfs:
                 line = line.replace(u'\xa0', u' ')
                 self.errors.write(line.to_csv(sep=' ', index=False, header=False))
 
-    def parse_tables(self):
+    def parse_tables(self):  # main driver for this program
         self.login()
 
         self.go_to_majors_page()
-        majors = self.parse_raw_majors()
+        majors = self.parse_raw_majors()  # major list
 
         for major in majors:
             self.go_to_courses_page(major)
             courses = self.parse_raw_courses(major)
 
-            for course in courses:
+            for course in courses:  # course list for each major
                 self.go_to_courses_description_page(course)
                 self.save_tables()
-                self.driver.back()  # due to the stability of connections, this may not success
+                self.driver.back()
 
             self.driver.back()
 
