4731a6b68a29ab85dab746a2145ba34344d6f590
djackso2 <djackso2@stevens.edu>
2017-11-14 20:23:28 -0600

    Merged prelim study plan with popup updates.

--- application/samainapp.py
+++ application/samainapp.py
@@ -1,23 +1,24 @@
 from kivy.app import App
 from kivy.network.urlrequest import UrlRequest
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager , Screen
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+import json
 
 # Base application for the SWAARJA Student Assistant Application
 # Authors: Dan Jackson, Alla Alharazi, Eileen Roberson
 # Modification of the prototype SA application written by Zach Ankuda
 
 Window.size = (600, 700)
-Window.clearcolor = (1, 1, 1, 1)
+Window.clearcolor = (1 , 1 , 1 , 1)
 
-focus_btn_color = (2, 0 ,0, 1)
-active_btn_color = (1, 0, 0, 1)
+focus_btn_color = (2 , 0 , 0 , 1)
+active_btn_color = (1 , 0 , 0 , 1)
 
 class HomeScreen(Screen):
     pass
@@ -31,64 +32,101 @@ class MyTextInput(TextInput):
         substring = substring[:6 - len(self.text)]
         return super(MyTextInput, self).insert_text(
             substring, from_undo=from_undo)
-
+            
+'''Backing class for the page for recording, saving, editing, and
+retrieving a degree course plan.
+'''
 class StudyPlanScreen(Screen):
-    pass
+    
+    '''Save Button handler that stores the currently entered 
+    study plan info as a JSON object and file on the local file 
+    system.
+    '''
+    def save_handler(self):        
+        study_plan = {}
+        study_plan['is_grad'] = self.ids.grad_checkbox.state        
+        for x in self.ids.stdypln_box.children:
+            study_plan[x.children[1].text] =  x.children[0].text
+        try:
+            with open('study_plan', 'w') as outfile:
+                json.dump(study_plan, outfile)      
+        except Exception as ex:
+            print('Study plan file write error: ', ex)
+            
+    '''Load button handler that retrieves the user's previously
+    saved study plan from the study_plan JSON object file.
+    '''
+    def load_handler(self):
+        try:
+            with open('study_plan') as json_data:
+                course_list = json.load(json_data)
 
-#Backing class for the main page for searching and viewing
-# information on courses.
+                if course_list['is_grad'] == 'down':
+                    self.ids.grad_checkbox.state = 'down'
+                    self.ids.undergrad_checkbox.state = 'normal'
+                else:
+                    self.ids.grad_checkbox.state = 'normal'
+                    self.ids.undergrad_checkbox.state = 'down'           
+                for key, value in course_list.items():
+                    for x in self.ids.stdypln_box.children:
+                         if x.children[1].text == key:
+                            x.children[0].text =  value                
+        except Exception as ex: 
+            print('Study plan file load error: ', ex)
+            
+    def reset_handler(self):
+        for x in self.ids.stdypln_box.children:
+            x.children[0].text = ''
+    
+
+'''Backing class for the main page for searching and viewing
+information on courses.
+'''
 class CoursesScreen(Screen):
-    '''Basic feature state of the courses screen.  Available states:
-    avail, desc, times
+    
+    '''Basic function state of the courses screen.  
     avail = crs_go_btn on_press handler will search for available
     courses based on the department taken from crs_search_txtIn.
-    desc = crs_go_btn on_press handler will search for a course
-    description based on the course ID taken from crs_search_txtIn.
     times = crs_go_btn on_press handler will search for course times
-    based on the course ID taken from crs_search_txtIn.'''
-
+    based on the course ID taken from crs_search_txtIn.
+    '''
     active_crs_state = 'avail'
 
+    '''Set state to Courses Available function ('avail')
+    '''
     def set_course_avail_state(self):
         self.ids.crs_srch_lbl.text = 'Department:'
         self.ids.crs_srch_txtin.text = 'ex. SSW'
         self.ids.avail_btn.background_color = focus_btn_color
         self.ids.times_btn.background_color = active_btn_color
-        self.ids.desc_btn.background_color = active_btn_color
         self.active_crs_state = 'avail'
-
+        
+    '''Set state to COurse Times function ('times')
+    '''
     def set_course_times_state(self):
         self.ids.crs_srch_lbl.text = 'Course ID:'
         self.ids.crs_srch_txtin.text = 'ex. SSW555'		
         self.ids.avail_btn.background_color = active_btn_color
         self.ids.times_btn.background_color = focus_btn_color
-        self.ids.desc_btn.background_color = active_btn_color
         self.active_crs_state = 'times'
-
-    def set_course_desc_state(self):
-        self.ids.crs_srch_lbl.text = 'Course ID:'
-        self.ids.crs_srch_txtin.text = 'ex. SSW555'
-        self.ids.avail_btn.background_color = active_btn_color
-        self.ids.times_btn.background_color = active_btn_color
-        self.ids.desc_btn.background_color = focus_btn_color
-        self.active_crs_state = 'desc'
-
-    def get_course_id_text(self):
-        #course_id TextInput text validation function
-        #@TODO Add additional text validation after
-        # coordinating with server team
-        def validate_course_id_text():
-            return(len(self.ids.crs_srch_txtin.text) == 6)
-
+    
+    '''Creates and displays a Popup object when a Course ID display
+    button is clicked. Displays the Course ID, Course Name, Course
+    Description, Course Time, Course Status.
+    '''
     def open_popup(self):
         the_popup = CustomPopup()
         the_popup.title = self.text
+        the_popup.auto_dismiss = True                                     
 
         req = self.parent.parent.parent.parent.parent.create_url_request(
             'desc', self.text)
         res = req.result[0]
         
-        layout = BoxLayout(orientation = 'vertical', spacing = 2, height = 1000, width= 900, pos_hint= {'center_x': 0.2, 'center_y': 0.5})
+        layout = BoxLayout(
+            orientation = 'vertical', spacing = 2,
+            height = 1000, width= 900,
+            pos_hint= {'center_x': 0.2, 'center_y': 0.5})
         #work on position pos_hint= {'right': 0.2, 'center_x': 1}
         a = Label (
             text = 'Course Name: ' + res['course_name'],
@@ -125,21 +163,22 @@ class CoursesScreen(Screen):
         
         the_popup.open()
 	
+    '''Initiates a create_url_request call to the server based on the
+    currently selected state of the Courses page and the value in the 
+    crs_srch_txtin. Displays the results in the crs_disp_box.
+    '''
     def go_btn_handler(self):
-        #@TODO: why doesn't this call work if the active_crs_state
-        # hasn't changed between calls to go_btn_handler?
         self.reset_crs_srch_box()
-        #display_limit = 7
         
-        '''Switch that populates the crs_disp_box differently
-        based on the CourseScreen active_crs_state'''
+        #Switch that populates the crs_disp_box differently
+        #based on the CourseScreen active_crs_state
         req = self.create_url_request(self.active_crs_state,
             self.ids.crs_srch_txtin.text)
         if len(req.result)>0:
             if self.active_crs_state == 'avail':                
-                '''Iterates over the results in the req object and
-                creates a set of button and labels for each result.
-                Adds these to the crs_disp_box display area.'''
+                #Iterates over the results in the req object and
+                #creates a set of button and labels for each result.
+                #Adds these to the crs_disp_box display area.
                 for x in range(0,len(req.result)):
                     res = req.result[x]
                     c_id_btn = Button(
@@ -154,19 +193,10 @@ class CoursesScreen(Screen):
                     self.ids.crs_disp_box.add_widget(c_id_btn)
                     self.ids.crs_disp_box.add_widget(c_name_label)
                     self.ids.crs_disp_box.add_widget(c_seats_label)
-                    '''Workaround to autoscrolling in cases when there
-                    are too many results to display on screen.'''
-#                    if x == display_limit:
-#                        c_limit_btn = Button(
-#                            text='TOO MANY COURSES',
-#                            background_color =(1.0, 0.0, 0.0, 1.0),
-#                            size_hint_y=(None), size=(100, 50))
-#                        self.ids.crs_disp_box.add_widget(c_limit_btn)
-#                        break
             elif self.active_crs_state == 'times':
-                '''Iterates over the results in the req object and
-                creates a set of button and labels for each result.
-                Adds these to the crs_disp_box display area.'''
+                #Iterates over the results in the req object and
+                #creates a set of button and labels for each result.
+                #Adds these to the crs_disp_box display area.'''
                 for x in range(0,len(req.result)):
                     res = req.result[x]
                     c_id_btn = Button(
@@ -181,18 +211,6 @@ class CoursesScreen(Screen):
                     self.ids.crs_disp_box.add_widget(c_id_btn)
                     self.ids.crs_disp_box.add_widget(c_name_label)
                     self.ids.crs_disp_box.add_widget(c_seats_label)
-                    '''Workaround to autoscrolling in cases when there
-                    are too many results to display on screen.'''
-#                    if x == display_limit:
-#                        c_limit_btn = Button(
-#                            text='TOO MANY COURSES',
-#                            background_color =(1.0, 0.0, 0.0, 1.0),
-#                            size_hint_y=(None), size=(100, 50))
-#                        self.ids.crs_disp_box.add_widget(c_limit_btn)
-#                        break
-                pass
-#            elif self.active_crs_state == 'desc':            
-#                pass
         else:
             c_name_label = Label(text='NO RESULTS',
                 color=(0, 0 ,0 ,1))
@@ -230,7 +248,6 @@ class CoursesScreen(Screen):
                 search = srch_id
 
         #Construct the URLRequest object
-        print(server+rpc+search)
         req = UrlRequest(
             server+rpc+search, 
         req_headers=headers)
@@ -238,7 +255,8 @@ class CoursesScreen(Screen):
         #return the URLRequest object	
         return req
 
-    #helper function to clear the display space
+    '''Clears the display box area.
+    '''
     def reset_crs_srch_box(self):
         self.ids.crs_disp_box.clear_widgets()
         #self.ids.crs_srch_txtin.text = ''
